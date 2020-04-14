30c34558ac59b39f428680113b1b7375905783e3
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-04-14 13:09:13 -0500

    index.html and htmlController set up so we can do heroku deployment

--- controllers/htmlController.js
+++ controllers/htmlController.js
@@ -1,1 +1,16 @@
 // root route - must be done before deployment to heroku.  just deploy a basic index.html with bootstrap
+
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+module.exports = router;
