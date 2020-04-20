5d413602beac3b633dbb7bf18dba4d1f07bf89d6
Julia <jlchen6@wisc.edu>
2020-04-20 01:03:32 -0500

    Added a couple comments to child model

--- models/child.js
+++ models/child.js
@@ -1,14 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
     const Child = sequelize.define("Child", {
     // create a Child model
+    // Child's full name
   fullName: {
     type: DataTypes.STRING,
     allowNull: false
   },
+  // Child's gender
   gender: {
     type: DataTypes.STRING,
     allowNull: false
   },
+  // Child's date of birth
   dob: {
     type: DataTypes.DATE,
     allowNull: false
